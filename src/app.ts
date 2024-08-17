e91405cb07a32e00371b3f597e1ce50bea6ea779
GuidoAmbiorix <guidoambiorix@gmail.com>
2024-08-17 11:52:55 -0400

    Se hicieron los controladores base de register y login y se empezo el middleware para los dtos

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("hola mundo!!");
+import { envs } from "./config";
+import { AppRoutes } from "./presentation/routes";
+import { Server } from "./presentation/server";
+
+(() => {
+  main();
+})();
+
+async function main() {
+  new Server({ port: envs.PORT, routes: AppRoutes.routes }).start();
+}
